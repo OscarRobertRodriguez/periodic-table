19a9a44a5d962ce766e3a9df2bbaaf531b564391
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-02-23 16:49:15 -0600

    put in strict mode and put code in iffe to prevent global pollution

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,28 @@
+"use strict";
 
+(function() {
 
 //get input
-let searchInput = document.querySelector('.input__search-input');
-let elements = [...document.querySelectorAll('.element__block')];
+	let searchInput = document.querySelector('.input__search-input');
+	let elements = [...document.querySelectorAll('.element__block')];
 
 // add keydown event to input
-searchInput.addEventListener("keyup", (e) => {
-	let inputValue = e.target.value.toLowerCase();
+	searchInput.addEventListener("keyup", (e) => {
+		let inputValue = e.target.value.toLowerCase();
 
 
-	elements.forEach(element => {
-		let elementName = element.firstElementChild.textContent;
-	  if(elementName.toLowerCase().indexOf(inputValue) != -1) {
-	  	element.style.opacity = 1;
-	  } else {
-	  	element.style.opacity = .5;
-	  }
+		elements.forEach(element => {
+			let elementName = element.firstElementChild.textContent;
+			if (elementName.toLowerCase().indexOf(inputValue) != -1) {
+				element.style.opacity = 1;
+			} else {
+				element.style.opacity = .5;
+			}
+		});
+
+
 	});
 
-
-});
+})();
 
 
