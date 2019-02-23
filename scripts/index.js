47638ac529a3c3032b87adb0457eadc8cf865332
Oscar Rodriguez <oscarrobertrodriguez@gmail.com>
2019-02-23 16:42:30 -0600

    add javascript for search bar

---
+++ scripts/index.js
@@ -0,0 +1,24 @@
+
+
+//get input
+let searchInput = document.querySelector('.input__search-input');
+let elements = [...document.querySelectorAll('.element__block')];
+
+// add keydown event to input
+searchInput.addEventListener("keyup", (e) => {
+	let inputValue = e.target.value.toLowerCase();
+
+
+	elements.forEach(element => {
+		let elementName = element.firstElementChild.textContent;
+	  if(elementName.toLowerCase().indexOf(inputValue) != -1) {
+	  	element.style.opacity = 1;
+	  } else {
+	  	element.style.opacity = .5;
+	  }
+	});
+
+
+});
+
+
